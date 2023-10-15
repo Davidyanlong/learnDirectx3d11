336502370e7bff2aa517c5caa69671d2347a4a23
yanlong.yang <yyl_love_you@126.com>
2023-10-15 20:52:48 +0800

    cosmetic twerks and comments

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -16,15 +16,16 @@ int CALLBACK WinMain(
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
-		TranslateMessage(&msg );
+		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
+		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
+	// check if GetMessage call itself borked
 	if (gResult == -1) {
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+	// wParam here is the value passed to PostQuitMessage
+	return msg.wParam;
+
 }
