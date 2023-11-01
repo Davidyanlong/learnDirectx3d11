17b45bf07d5fdd52032ec847d80f26ef3a3e7c81
yanlong.yang <yyl_love_you@126.com>
2023-11-01 21:31:35 +0800

    feat： simple rotation

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -98,7 +98,7 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	pContext->ClearRenderTargetView(pTarget.Get(), color);
 }
 
-void Graphics::DrawTestTriangle()
+void Graphics::DrawTestTriangle( float angle )
 {
 	namespace wrl = Microsoft::WRL;
 	HRESULT hr;
@@ -157,6 +157,7 @@ void Graphics::DrawTestTriangle()
 	const UINT offset = 0u;
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
 
+	// create index buffer
 	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
 	D3D11_BUFFER_DESC ibd = {};
 	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
@@ -170,6 +171,42 @@ void Graphics::DrawTestTriangle()
 	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
 
 	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+
+	// create constant buffer for transformation matrix
+	struct ConstantBuffer
+	{
+		struct
+		{
+			float element[4][4];
+		  } transformation;
+	};
+
+	// ÈÆ Z ÖáÐý×ª
+	const ConstantBuffer cb =
+	{
+		(3.0f/4.0f) * std::cos(angle), std::sin(angle), 0.0f,   0.0f,
+		(3.0f / 4.0f) * -std::sin(angle), std::cos(angle), 0.0f,  0.0f,
+		0.0f,                     0.0f,                    1.0f,  0.0f,
+		0.0f,                     0.0f,                     0.0f, 1.0f,
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd = {};
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof(cb);
+	cbd.StructureByteStride = sizeof(Vertex);
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&cbd, &csd, &pConstantBuffer));
+
+
+	// bind constant buffer to vertex shader
+	pContext->VSSetConstantBuffers(0u, 1u, pConstantBuffer.GetAddressOf());
+
+
 
 	// create pixel shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
