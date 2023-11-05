388f4b2d1403d8bac8f90ef7ba2bc4efb79cfee8
yanlong.yang <yyl_love_you@126.com>
2023-11-05 11:54:30 +0800

    geometric primitives

--- hw3d/PixelShader.hlsl
+++ hw3d/PixelShader.hlsl
@@ -6,5 +6,5 @@ cbuffer CBuf
 // If you get following error Error X4583 semantic 'SV_PrimitiveID' unsupported on ps_4_0_level_9_3 Please specify pixelShader properties -> HLSL compiler -> General -> Shader model -> Shader Model 4 (/4_0)
 float4 main(uint tid : SV_PrimitiveID) : SV_Target
 {
-    return face_colors[tid/ 2];
+    return face_colors[(tid / 2) % 6];
 }
