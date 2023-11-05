55a89afcd504f5c57d50edf945963068ebcc1e1e
yanlong.yang <yyl_love_you@126.com>
2023-11-05 09:22:56 +0800

    fix: 更改shader 配置可以正常运行
    If you get following error Error X4583 semantic 'SV_PrimitiveID' unsupported on ps_4_0_level_9_3 Please specify pixelShader properties -> HLSL compiler -> General -> Shader model -> Shader Model 4 (/4_0)

--- hw3d/PixelShader.hlsl
+++ hw3d/PixelShader.hlsl
@@ -3,7 +3,8 @@ cbuffer CBuf
     float4 face_colors[6];
 };
 
+// If you get following error Error X4583 semantic 'SV_PrimitiveID' unsupported on ps_4_0_level_9_3 Please specify pixelShader properties -> HLSL compiler -> General -> Shader model -> Shader Model 4 (/4_0)
 float4 main(uint tid : SV_PrimitiveID) : SV_Target
 {
-    return face_colors[tid / 2];
+    return face_colors[tid/ 2];
 }
